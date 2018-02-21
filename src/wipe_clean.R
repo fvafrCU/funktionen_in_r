6b886a318ab84e459a47c4c9a479644ae5f95e79
Dominik Cullmann <dominik.cullmann@forst.bwl.de>
2018-02-21 11:18:42 +0100

    Uncommented changes: backing up

---
+++ src/wipe_clean.R
@@ -0,0 +1,11 @@
+#' Remove All Objects From an Environment
+#'
+#' @param environment The environment to be wiped.
+#' @return A character vector containing the names of objects removed.
+#' @export
+wipe_clean <- function(environment = .GlobalEnv) {
+    objects <- ls(name = environment, all.names = TRUE)
+    rm(list = objects, envir = environment)
+    return(invisible(objects))
+}
+
