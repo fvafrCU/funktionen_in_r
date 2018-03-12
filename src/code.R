5094b36367810e16f4af6effafec36c06f6ecc11
fvafrCU <fvafrcu@arcor.de>
2018-03-12 23:31:04 +0100

    cleanr, lintr, cyclocomp

---
+++ src/code.R
@@ -0,0 +1,25 @@
+foo <- function(this, func, has, way, too, many, args) {
+    if (TRUE) {
+        if (TRUE) {
+            if (TRUE) {
+                if (TRUE) {
+                    if (TRUE) {
+                        print(foo)
+                    } else {
+                        print(bar)
+                    }
+                } else {
+                    print(bar)
+                }
+            } else {
+                print(bar)
+            }
+        } else {
+            print(bar)
+        }
+    } else {
+        print(bar)
+    }
+    return(invisible(NULL))
+}
+
