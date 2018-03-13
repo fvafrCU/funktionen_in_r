3dc97f0513d8af9912eea7c02ebbd792f58cbe5f
fvafrCU <fvafrcu@arcor.de>
2018-03-13 11:34:43 +0100

    linting devtools

---
+++ src/test_wipe_clean_broken.R
@@ -0,0 +1,7 @@
+testthat::test_that("Test return value.", {
+                    a <- 3
+                    testthat::expect_identical(wipe_clean(), 
+                                               c("a"))}
+                    )
+
+
---
+++ src/wipe_clean_broken.R
@@ -0,0 +1,11 @@
+#' Remove All Objects From an Environment
+#'
+#' @param environment The environment to be wiped.
+#' @return A character vector containing the names of objects removed.
+#' @export
+wipe_clean <- function(environment = parent.frame()) {
+    objects <- ls(name = environment, all.names = TRUE)
+    rm(list = objects)
+    return(invisible(objects))
+}
+
