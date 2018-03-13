34d8d01a103d6aea849b8eecddad6e13c750be11
fvafrCU <fvafrcu@arcor.de>
2018-03-13 10:04:14 +0100

    Uncommented changes: backing up

--- src/code.R
+++ src/code.R
@@ -4,21 +4,25 @@ foo <- function(this, func, has, way, too, many, args) {
             if (TRUE) {
                 if (TRUE) {
                     if (TRUE) {
-                        print(foo)
+                        print("Foo.")
                     } else {
-                        print(bar)
+                        stop("Foobar!")
                     }
                 } else {
-                    print(bar)
+                    stop("Foobar!")
                 }
             } else {
-                print(bar)
+                if (TRUE) {
+                    print("Foo.")
+                } else {
+                    stop("Foobar!")
+                }
             }
         } else {
-            print(bar)
+            stop("Foobar!")
         }
     } else {
-        print(bar)
+        stop("Foobar!")
     }
     return(invisible(NULL))
 }
