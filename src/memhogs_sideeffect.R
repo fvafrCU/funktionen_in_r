fb3b3b80d6188b518066ae4c66710c757c3f3b6b
fvafrCU <fvafrcu@arcor.de>
2018-03-15 14:44:29 +0100

    get rid of stale soure

--- src/memhogs_sideeffect.R
+++
@@ -1,8 +0,0 @@
-memory_hogs <- function(verbose = TRUE) {
-    z <- sapply(ls(envir = parent.frame()), 
-                function(x) object.size(get(x, envir = parent.frame())))
-    z <- z[order(z, decreasing = TRUE)]
-    if (isTRUE(verbose)) print(z)
-    return(invisible(names(z)))
-}
-
