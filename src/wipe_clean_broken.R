4d07495450245dc70d18c9ab33957797296ff3eb
fvafrCU <fvafrcu@arcor.de>
2018-03-15 14:41:47 +0100

    Uncommented changes: backing up

--- src/wipe_clean_broken.R
+++ src/wipe_clean_broken.R
@@ -1,8 +1,3 @@
-#' Remove All Objects From an Environment
-#'
-#' @param environment The environment to be wiped.
-#' @return A character vector containing the names of objects removed.
-#' @export
 wipe_clean <- function(environment = parent.frame()) {
     objects <- ls(name = environment, all.names = TRUE)
     rm(list = objects)
