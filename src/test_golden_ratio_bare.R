c536a923f5baae09571bc52a9f05b302d0045a6e
fvafrCU <fvafrcu@arcor.de>
2018-03-19 13:05:26 +0100

    Uncommented changes: backing up

--- src/test_golden_ratio_bare.R
+++ src/test_golden_ratio_bare.R
@@ -1,6 +1,6 @@
-testthat::test_that("Test on golden rectangle", {
+testthat::test_that("Test on Golden Ratio", {
                         expectation <- (1 + sqrt(5)) / 2
-                        result <- golden_ratio(value)
+                        result <- golden_ratio()
                         testthat::expect_identical(result, expectation)
 })
 
