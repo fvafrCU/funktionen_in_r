ce9046f0df3833f20310f032c99c7603995fac46
fvafrCU <fvafrcu@arcor.de>
2018-03-21 11:36:53 +0100

    Uncommented changes: backing up

---
+++ src/.golden_ratio_doc.R
@@ -0,0 +1,9 @@
+#' Compute the Golden Ratio
+#'
+#' See \url{https://en.wikipedia.org/wiki/Golden_ratio} for an introduction to
+#' the Golden Ratio.
+#'
+#' @return The Golden Ratio
+#' @keywords internal
+#' @examples
+#' print(.golden_ratio())
