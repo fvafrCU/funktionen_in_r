8cd4bae586ff1225a0506cbec8552bff5ad1808a
fvafrCU <fvafrcu@arcor.de>
2018-04-24 09:38:37 +0200

    add context to testthat test file
    is needed for newer testthat reporter versions

--- src/test_my_sum.R
+++ src/test_my_sum.R
@@ -1,3 +1,4 @@
+testthat::context("My first test.")
 testthat::test_that("Simple test on summation.", {
                         testthat::expect_identical(not_my_sum(c(2, 7)), 9,
                                                    info = "Not my sum.")
