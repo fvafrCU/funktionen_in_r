4ff82d6fac528bd554c3e3c7250f3b55f04d81a9
fvafrCU <fvafrcu@arcor.de>
2018-06-08 09:52:16 +0200

    add comment to broken function

--- src/wipe_clean_broken.R
+++ src/wipe_clean_broken.R
@@ -1,6 +1,8 @@
 wipe_clean <- function(environment = parent.frame()) {
     objects <- ls(name = environment, all.names = TRUE)
-    rm(list = objects)
+    # FIXME: rm() by default removes objects from  
+    # the current frame, which is this function, not the parent frame!
+    rm(list = objects) 
     return(invisible(objects))
 }
 
