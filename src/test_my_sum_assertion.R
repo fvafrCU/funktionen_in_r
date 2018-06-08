107b494937886f96a3bce7ca15bbcd851fb3345a
fvafrCU <fvafrcu@arcor.de>
2018-06-08 09:38:18 +0200

    fix file name

--- src/test_my_sum_assertion.R
+++ src/test_my_sum_assertion.R
@@ -1,3 +1,5 @@
-testthat::test_that("Simple test on summation.",
+testthat::context("Tests for summation with argument assertion.")
+testthat::test_that("Testing summation", {
                     testthat::expect_identical(my_sum(c(2, 7)), 9)
-                    )
+                    testthat::expect_error(my_sum(c("2" + "7")))
+})
